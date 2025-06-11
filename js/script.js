768b72d36bc0d37a2e36f5f267a707ebb917c521
Laxman Goud <codewithlaxman92@gmail.com>
2025-06-11 09:33:12 +0530

    play pause functionality

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 let audio = new Audio();
+let playPause = document.querySelector('.play');
+let mainPlayEl = document.querySelector('.play')
 
 async function getSongs() {
     let a = await fetch("http://127.0.0.1:5500/songs/");
@@ -46,13 +48,16 @@ async function main() {
 
     document.querySelectorAll('.play').forEach(element => {
         element.addEventListener('click', () => {
+            playPause.src = 'img/play.svg';
+            mainPlayEl.src = 'img/play.svg';
             audio.pause();
             const songName = element.parentElement.querySelector('.song-name').innerText.trim();
             const songArtist = element.parentElement.querySelector('.song-artist').innerText.trim();
+            playPause = element;
             playSong(`${songName} - ${songArtist}`);
         });
     });
-}
+} 
 
 main();
 
@@ -60,5 +65,7 @@ function playSong(songFileName) {
     const songUrl = `http://127.0.0.1:5500/songs/${encodeURIComponent(songFileName)}.mp3`;
     console.log("Playing:", songUrl);
     audio = new Audio(songUrl);
+    playPause.src = 'img/pause.svg';
+    mainPlayEl.src = 'img/pause.svg';
     audio.play();
 }
