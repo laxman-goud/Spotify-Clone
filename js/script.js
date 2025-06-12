13d8e2071027dfeaa901294d273d0844443437ad
Laxman Goud <codewithlaxman92@gmail.com>
2025-06-12 22:51:02 +0530

    added updatePlayBar, formatTime Functions

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 let audio = new Audio();
 let playPause = document.querySelector('.play');
-let mainPlayEl = document.querySelector('.play')
+let mainPlayEl = document.querySelector('.play');
 
 async function getSongs() {
     let a = await fetch("http://127.0.0.1:5500/songs/");
-    let response = await a.text(); 
+    let response = await a.text();
     let div = document.createElement('div');
     div.innerHTML = response;
-    
+
     let as = div.getElementsByTagName('a');
-    
+
     let songs = [];
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
@@ -17,7 +17,7 @@ async function getSongs() {
             songs.push(element.href);
         }
     }
-    
+
     return songs;
 }
 
@@ -48,20 +48,30 @@ async function main() {
 
     document.querySelectorAll('.play').forEach(element => {
         element.addEventListener('click', () => {
-            if(audio.paused){
-                const songName = element.parentElement.querySelector('.song-name').innerText.trim();
-                const songArtist = element.parentElement.querySelector('.song-artist').innerText.trim();
+            const songName = element.parentElement.querySelector('.song-name').innerText.trim();
+            const songArtist = element.parentElement.querySelector('.song-artist').innerText.trim();
+            const fullSongName = `${songName} - ${songArtist}`;
+
+            if (audio.paused || audio.src.indexOf(fullSongName) === -1) {
                 playPause = element;
-                playSong(`${songName} - ${songArtist}`);
-            }
-            else{
+                playSong(fullSongName); 
+                audio.addEventListener('loadedmetadata', () => {
+                    const duration = formatTime(audio.duration);
+                    updatePlayBar(songName, songArtist, duration);
+                }, { once: true }); 
+
+                playPause.src = 'img/pause.svg';
+                mainPlayEl.src = 'img/pause.svg';
+            } else {
                 audio.pause();
                 playPause.src = 'img/play.svg';
                 mainPlayEl.src = 'img/play.svg';
             }
         });
     });
-} 
+
+
+}
 
 main();
 
@@ -72,4 +82,18 @@ function playSong(songFileName) {
     playPause.src = 'img/pause.svg';
     mainPlayEl.src = 'img/pause.svg';
     audio.play();
+}
+
+function updatePlayBar(songName, songArtist, songDuration) {
+    console.log(songName,songArtist,songDuration);
+    let playBar_songName = document.querySelector('.playBar-song-name');
+    let playBar_songDuration = document.querySelector('.song-duration');
+    playBar_songName.innerText = songName + ' - ' + songArtist;
+    playBar_songDuration.innerText = songDuration;
+}  
+
+function formatTime(seconds) {
+    let min = Math.floor(seconds / 60);
+    let sec = Math.floor(seconds % 60);
+    return `${min}:${sec < 10 ? '0' + sec : sec}`;
 }
